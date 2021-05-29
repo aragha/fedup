96feb30cc1f0ed4ec13f1e225b7b38f2a23d7b8d
feduser <aras2k@yahoo.com>
2021-05-29 17:58:05 -0400

    updated to remove --assumeyes on the "sudo rpm" command line

--- brave.sh
+++ brave.sh
@@ -1,6 +1,6 @@
 sudo dnf remove brave-browser --assumeyes
 sudo dnf install dnf-plugins-core --assumeyes
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/ --assumeyes
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc --assumeyes
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc 
 sudo dnf install brave-browser --assumeyes
 sudo dnf clean packages
