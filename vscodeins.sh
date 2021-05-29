0435ff527ac662477315b40204938994c8f3cfd7
feduser <aras2k@yahoo.com>
2021-05-29 17:23:12 -0400

    added conditional install of snapd

--- vscodeins.sh
+++ vscodeins.sh
@@ -1,12 +1,18 @@
 #instructions found in https://snapcraft.io/install/code/fedora
 
-#install and enable snapd
-sudo dnf install snapd --assumeyes
+dnf list installed | grep "snapd" > fc
+if grep 'snapd' "./fc"; then
+        echo 'snapd installed'
+else
+	#install and enable snapd
+        sudo dnf install snapd --assumeyes
+fi
 
 #enable classic snap support - create a symbolic link between
 # /var/lib/snapd/snap and /snap
 sudo ln -s /var/lib/snapd/snap /snap 
 
+sudo reboot now
 #install visual studio code
 sudo snap install code --classic
 
