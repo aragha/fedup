ee1f97e40cae9c3ab1f3a6279205b292fe3a1c4b
feduser <aras2k@yahoo.com>
2021-06-05 21:04:52 -0400

    added comment

--- unz.sh
+++ unz.sh
@@ -1,1 +1,5 @@
 unzip mle.zip
+#check links below for more info.:
+#Latest sources and executables are at ftp://ftp.info-zip.org/pub/infozip,
+#as of above date; see http://www.info-zip.org/ for other sites.
+
